fa647b79d7b8ffeec2eca6820eb14adf72addcc7
sachix1001 <sachix1001@gmail.com>
2020-02-10 17:03:16 +0900

    add api GET

---
+++ server/app.js
@@ -0,0 +1,35 @@
+// server/app.js
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const db = require("./knex");
+
+const app = express();
+
+// Setup logger
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
+  )
+);
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.get("/api/movies", async (req, res) => {
+  try {
+    const movies = await db.select().table("movies");
+    res.json(movies);
+  } catch (err) {
+    console.error("Error loading locations!", err);
+    res.sendStatus(500);
+  }
+});
+
+
+// Always return the main index.html, so react-router render the route in the client
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+});
+
+module.exports = app;
