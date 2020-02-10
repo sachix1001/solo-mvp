66385cc784f9ddb57ab4dd970e2c97d1c8022cc4
sachix1001 <sachix1001@gmail.com>
2020-02-10 21:56:45 +0900

    add image

--- data/data.js
+++ data/data.js
@@ -3,126 +3,126 @@ const Ghibli = [
     title: "When Marnie Was There",
     content:
       "2014 Hiromasa Yonebayashi	Hiromasa Yonebayashi, Keiko Niwa & Masashi Ando	Yoshiaki Nishimura & Toshio Suzuki	Takatsugu Muramatsu      anime drama family mistery",
-    img: "21marnie.jpg"
+    img: "marnie21"
   },
   {
     title: "The Wind Rises",
     content:
       "2013 Hayao Miyazaki Toshio Suzuki Joe Hisaishi and Aoi Teshima anime drama war romance historical fiction",
-    img: "20kazetachinu.jpg"
+    img: "kazetachinu20"
   },
   {
     title: " The Tale of the Princess Kaguya",
     content:
       "2013 Isao Takahata Riko Sakaguchi Joe Hisaishi and Aoi Teshima anime drama fantasy adventure coming-of-age fiction family",
-    img: "19kaguya.jpg"
+    img: "kaguya19"
   },
   {
     title: "From Up on Poppy Hill",
     content:
       "2011 Gorō Miyazaki Hayao Miyazaki & Keiko Niwa Satoshi Takebe  Toshio Suzukianime romance drama family",
-    img: "18Kokuriko.jpg"
+    img: "Kokuriko18"
   },
   {
     title: "Arrietty",
     content:
       "2010 Hiromasa Yonebayashi Hayao Miyazaki  Keiko Niwa Toshio Suzuki Cécile Corbel anime adventure family fantasy",
-    img: "17arrietty.jpg"
+    img: "arrietty17"
   },
   {
     title: "Ponyo",
     content:
       "2008 Hayao Miyazaki Toshil suzuki Joe Hisaishi Toshio Suzuki anime adventure comedy family fantasy drama",
-    img: "16ponyo.jpg"
+    img: "ponyo16"
   },
   {
     title: "Tales from Earthsea",
     content:
       "2006 Gorō Miyazaki Keiko Niwa Tamiya Terashima Toshio Suzuki anime drama adventure fantasy",
-    img: "15gedo.jpg"
+    img: "gedo15"
   },
   {
     title: "Howl's Moving Castle",
     content:
       "2004 Hayao Miyazaki Toshio Suzuki Joe Hisaishi Anime Romance Adventure Action Fantasy Drama Family",
-    img: "14howl.jpg"
+    img: "howl14"
   },
   {
     title: "The Cat Returns",
     content:
       "2002 Hiroyuki Morita Reiko Yoshtitlea Nozomu Takahashi & Toshio Suzuki Yuji Nomi anime romance adventure comedy fantasy family drama",
-    img: "13NekonoOngaeshi.jpg"
+    img: "NekonoOngaeshi13"
   },
   {
     title: "Spirited Away",
     content:
       "2001 Hayao Miyazaki Joe Hisaishi Toshio Suzuki Joe Hisaishi anime adventure family fantasy mystery coming-of-age fiction",
-    img: "12spirited.jpg"
+    img: "spirited12"
   },
   {
     title: "My Neighbors the Yamadas",
     content: "1999 Isao Takahata Toshio Suzuki Akiko Yano anime comedy family",
-    img: "11yamada.jpg"
+    img: "yamada11"
   },
   {
     title: "Princess Mononoke",
     content:
       "1997 Hayao Miyazaki Toshio Suzuki Joe Hisaishi anime adventure action epic fantasy drama historical fiction",
-    img: "10mononoke.jpg"
+    img: "mononoke10"
   },
   {
     title: "Whisper of the Heart",
     content:
       "1995 Yoshifumi Kondō	Hayao Miyazaki Toshio Suzuki Yuji Nomi anime romance adventure family drama music genre coming-of-age fiction",
-    img: "9Mimiwosumaseba.jpg"
+    img: "Mimiwosumaseba9"
   },
   {
     title: "Pom Poko",
     content:
       "1994 Isao Takahata Toshio Suzuki Shang Shang Typhoon anime family comedy",
-    img: "8ponpoko.jpg"
+    img: "ponpoko8"
   },
   {
     title: "Porco Rosso",
     content:
       "1992 Hayao Miyazaki Toshio Suzuki Joe Hisaishi anime romance adventure comedy fantasy magical realism",
-    img: "7rosso.jpg"
+    img: "rosso7"
   },
   {
     title: "Only Yesterday",
     content:
       "1991 Isao Takahata Toshio Suzuki Katz Hoshi anime romance drama melodrama coming-of-age fiction",
-    img: "6omotitlee.jpg"
+    img: "omotitlee6"
   },
   {
     title: "Kiki's Delivery Service",
     content:
       "1989 Hayao Miyazaki Joe Hisaishi anime adventure fantasy family drama",
-    img: "5Kiki.jpg"
+    img: "Kiki5"
   },
   {
     title: "My Neighbor Totoro",
     content:
       "1988	 Hayao Miyazaki Toru Hara Joe Hisaishi anime family fantasy adventure",
-    img: "4totoro.jpg"
+    img: "totoro4"
   },
   {
     title: "Grave of the Fireflies",
     content:
       "1988	 Isao Takahata Toru Hara Michio Mamiya anime war drama family",
-    img: "3Hotaru.jpg"
+    img: "Hotaru3"
   },
   {
     title: "Castle in the Sky",
     content:
       "1986 Hayao MiyazakiIsao Takahata	Joe Hisaishi anime romance adventure action fantasy family steampunk",
-    img: "2rapyuta.jpg"
+    img: "rapyuta2"
   },
   {
     title: "Nausicaä of the Valley of the Wind",
     content:
       "1984 Hayao Miyazaki Hayao Miyazaki	Isao Takahata Joe Hisaishi anime adventure action fantasy science fiction drama science fantasy epic",
-    img: "1naushika.jpg"
+    img: "naushika1"
   }
 ];
 
--- src/Movies.js
+++ src/Movies.js
@@ -3,18 +3,62 @@ import "./App.css";
 import { setAllMovies, selectMovie, allExceptSelected } from "./redux/redux";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
-import image from './images/1naushika.jpg'
+import {
+  naushika1,
+  rapyuta2,
+  Hotaru3,
+  totoro4,
+  Kiki5,
+  omoide6,
+  rosso7,
+  ponpoko8,
+  Mimiwosumaseba9,
+  mononoke10,
+  yamada11,
+  spirited12,
+  NekonoOngaeshi13,
+  howl14,
+  gedo15,
+  ponyo16,
+  arrietty17,
+  Kokuriko18,
+  kaguya19,
+  kazetachinu20,
+  marnie21
+} from "./images/index";
 
 function Movies() {
   const allMovies = useSelector(state => state.allMovies);
   const dispatch = useDispatch();
-
+  const image = {
+    naushika1,
+    rapyuta2,
+    Hotaru3,
+    totoro4,
+    Kiki5,
+    omoide6,
+    rosso7,
+    ponpoko8,
+    Mimiwosumaseba9,
+    mononoke10,
+    yamada11,
+    spirited12,
+    NekonoOngaeshi13,
+    howl14,
+    gedo15,
+    ponyo16,
+    arrietty17,
+    Kokuriko18,
+    kaguya19,
+    kazetachinu20,
+    marnie21
+  };
 
   return (
     <div className="App">
-      {allMovies.map(movie=>(
-          <img src={image} key='movie.id'/>
-      ))}
+      {allMovies.map(movie => {
+        return <img src={image[movie.img]} key="movie.id" />;
+      })}
     </div>
   );
 }
