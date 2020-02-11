2ffb0cf9c6a4c92513d71c300c33c24b0b43e163
sachix1001 <sachix1001@gmail.com>
2020-02-11 11:47:04 +0900

    change api end point

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, "..", "build")));
 
-app.get("/api/movies", async (req, res) => {
+app.get("//api/movies", async (req, res) => {
   try {
     const movies = await db.select().table("movies");
     res.json(movies);
